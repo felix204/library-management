4bb30d3ebdf592f7d73e647e117d7468dc2bffc6
felix204 <61236385+felix204@users.noreply.github.com>
2025-02-23 20:36:49 +0300

    feat: Add redirect for authenticated users on login page

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -3,11 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
 import { userLogin } from '@/api';
 import AuthForm from '@/components/AuthForm';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
     const dispatch = useDispatch();
     const router = useRouter();
-    const { loading, error } = useSelector((state) => state.auth);
+    const { loading, error, isAuthenticated } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            router.push('/');
+        }
+    }, [isAuthenticated, router]);
 
     const handleLogin = (formData) => {
         dispatch(userLogin(formData));
